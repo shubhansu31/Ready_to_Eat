619c4564dbd07d6713dbf7c0266f41019939e4e5
shubhansu31 <jainshubhansu31@gmail.com>
2021-12-17 19:40:55 +0530

    login screen added :

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,17 +5,24 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 const cartItemFromStorage = localStorage.getItem('cartItem') ? JSON.parse(localStorage.getItem('cartItem')) : []
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItem: cartItemFromStorage }
+    cart: { cartItem: cartItemFromStorage },
+    userLogin: { useerInfo: userInfoFromStorage }
 }
+
 
 const middleware = [thunk]
 
