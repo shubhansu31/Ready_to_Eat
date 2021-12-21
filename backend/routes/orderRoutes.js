dba15cc318118a8c872a790f40e30cb49a3d91c0
shubhansu31 <jainshubhansu31@gmail.com>
2021-12-21 22:52:29 +0530

    view all orders added

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -8,12 +8,12 @@ import {
     getMyOrders,
     getOrders,
 } from '../controllers/orderController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').post(protect, addOrderItems).get(protect, getOrders)
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
-router.route('/:id/pay').put(protect, updateOrderToPaid)
-router.route('/:id/deliver').put(protect, updateOrderToDelivered)
+router.route('/:id/pay').put(protect, admin, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 export default router
